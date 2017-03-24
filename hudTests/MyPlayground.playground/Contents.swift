c314f608436396b25eb60d5d8adfd023407808fe
hao yin <haoyin@haodeiMac.local>
2017-03-24 18:13:45 +0800

    playground

---
+++ hudTests/MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.liveView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+
